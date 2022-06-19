b903243f2696a33271c7181d245eb3944ae85841
Brandon Dith-Berry <brandondithberry@gmail.com>
2022-06-19 13:52:49 -0500

    Order

--- pages/menu.vue
+++ pages/menu.vue
@@ -1,12 +1,26 @@
 <template>
   <main>
     <h1>Our Famous Menu</h1>
-    <div v-for="post in posts" :key="post">
-      <h2>{{ post.category }}</h2>
+    <div class="grid grid-cols-3 gap-5">
+      <div v-for="(post, index) in posts" :key="index">
+        <h2>{{ post.category }}</h2>
+        <div v-for="(item, index) in post.item" :key="index">
+          <h4>{{ item.name }} - {{ item.price }}</h4>
+          <p>{{ item.description }}</p>
+        </div>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const posts = await $content('menu').fetch()
+
+    return {
+      posts,
+    }
+  },
+}
 </script>
