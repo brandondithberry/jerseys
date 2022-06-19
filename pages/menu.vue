2a1aee9966831effbd616ff6ec328140a0b03656
Brandon Dith-Berry <brandondithberry@gmail.com>
2022-06-19 13:05:31 -0500

    Update

---
+++ pages/menu.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Our Famous Menu</h1>
+    <div v-for="post in posts" :key="post">
+      <h2>{{ post.category }}</h2>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
